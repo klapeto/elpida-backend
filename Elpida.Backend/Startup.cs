8fc68d7f5496b9e56997ba03c531d62d6f4a6630
klapeto <yiannispana@hotmail.com>
2020-07-12 00:59:19 +0300

    Cors Configuration

--- Elpida.Backend/Startup.cs
+++ Elpida.Backend/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Elpida.Backend.Data;
 using Elpida.Backend.Data.Abstractions;
 using Elpida.Backend.Services;
 using Elpida.Backend.Services.Abstractions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Elpida.Backend
@@ -37,10 +29,12 @@ namespace Elpida.Backend
 
 			services.AddSingleton<IElpidaDatabaseSettings>(sp =>
 				sp.GetRequiredService<IOptions<ElpidaDatabaseSettings>>().Value);
-			
+
 			services.AddScoped<IResultsService, ResultService>();
 			services.AddSingleton<IResultsRepository, MongoResultsRepository>();
 			services.AddApiVersioning();
+
+			services.AddCors();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,7 +45,8 @@ namespace Elpida.Backend
 				app.UseDeveloperExceptionPage();
 			}
 
-			app.UseCors();
+			app.UseCors(builder =>
+				builder.WithOrigins("https://beta.elpida.dev", "https://elpida.dev").WithMethods("GET"));
 
 			app.UseHttpsRedirection();
 
