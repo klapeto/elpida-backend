473ef33f3a1db17339159b391062542a2f77924d
klapeto <yiannispana@hotmail.com>
2020-09-06 17:36:06 +0300

    Added missing action for backward Compatibilty

--- src/Elpida.Backend/Controllers/ResultController.cs
+++ src/Elpida.Backend/Controllers/ResultController.cs
@@ -69,16 +69,34 @@ namespace Elpida.Backend.Controllers
 		public async Task<IActionResult> GetSingle([FromRoute] string id, CancellationToken cancellationToken)
 		{
 			var resultDto = await _resultsService.GetSingleAsync(id, cancellationToken);
-			if (resultDto != null) return Ok(resultDto);
+			if (resultDto != null)
+			{
+				return Ok(resultDto);
+			}
+
 			return NotFound(id);
 		}
 
-		[HttpPost("search")]
+		[HttpGet]
+		[Produces("application/json")]
+		[Consumes("application/json")]
 		[ProducesResponseType(StatusCodes.Status200OK)]
 		[ProducesResponseType(StatusCodes.Status404NotFound)]
 		[ProducesResponseType(StatusCodes.Status400BadRequest)]
+		public async Task<IActionResult> GetPaged([FromQuery] PageRequest pageRequest,
+			CancellationToken cancellationToken)
+		{
+			return Ok(await _resultsService.GetPagedAsync(new QueryRequest {PageRequest = pageRequest},
+				cancellationToken));
+		}
+
+
+		[HttpPost("search")]
 		[Produces("application/json")]
 		[Consumes("application/json")]
+		[ProducesResponseType(StatusCodes.Status200OK)]
+		[ProducesResponseType(StatusCodes.Status404NotFound)]
+		[ProducesResponseType(StatusCodes.Status400BadRequest)]
 		public async Task<IActionResult> Search([FromBody] QueryRequest queryRequest,
 			CancellationToken cancellationToken)
 		{
